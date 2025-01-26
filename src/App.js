6044f4e7c8edf98b95f8334d97eac94970efb5b4
SamAI10 <noureddine.houssam.92@gmail.com>
2025-01-26 19:54:50 +0100

    Add App and main components

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import Invoices from './pages/Invoices';
 import Clients from './pages/Clients';
+import { ClientProvider } from './contexts/ClientContext';
+import { InvoiceProvider } from './contexts/InvoiceContext';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/invoices" element={<Invoices />} />
-            <Route path="/clients" element={<Clients />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <ClientProvider>
+      <InvoiceProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-50">
+            <Navbar />
+            <main className="container mx-auto px-4 py-8">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/clients" element={<Clients />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </InvoiceProvider>
+    </ClientProvider>
   );
 }
 
